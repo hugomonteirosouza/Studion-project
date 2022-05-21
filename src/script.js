81c7eef7c1f20439f04341ae9e55b3d9db5ffe58
Hugo Monteiro <hugaodebanquete@gmail.com>
2022-05-21 15:03:52 -0300

    feat(Mobile): implemented the mobile  responsibiity on homepage.

--- src/script.js
+++ src/script.js
@@ -1,5 +1,13 @@
 $(document).ready( () => {
-    
+
+    //MENU
+    $(".navbar__menu-btn").on('click' , function() {
+        $(".navbar__links").toggleClass("active");
+        $(this).find('i').toggleClass("fa-solid fa-bars-staggered");
+        $(this).find('i').toggleClass("fa-solid fa-xmark");
+    });
+
+    // CAROUSEL    
    const slickOptions = {
           autoplay:true,
           dots: false,
